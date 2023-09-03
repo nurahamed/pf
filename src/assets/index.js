5ad4902e86bad9e62e07bfd8dcfb2859092d1d28
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-09-04 02:37:23 +0530

    fix all

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,13 +1,12 @@
 import logo from "./images/logo.png";
-// import bannerImg from "./images/bannerImg.png";
 import bannerImg from "./images/bannerImg.svg";
 import projectOne from "./images/projectOne.png";
 import projectTwo from "./images/projectTwo.png";
 import projectThree from "./images/projectThree.png";
 import projectFour from "./images/ProjectFour.jpeg";
-import testimonialOne from "./images/testimonialOne.png";
-import testimonialTwo from "./images/testimonialTwo.png";
-import quote from "./images/quote.png";
+// import testimonialOne from "./images/testimonialOne.png";
+// import testimonialTwo from "./images/testimonialTwo.png";
+// import quote from "./images/quote.png";
 import contactImg from "./images/contact.png";
 
 export {
@@ -17,8 +16,8 @@ export {
   projectTwo,
   projectThree,
   projectFour,
-  testimonialOne,
-  testimonialTwo,
-  quote,
+  // testimonialOne,
+  // testimonialTwo,
+  // quote,
   contactImg,
 };
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { logo } from "../../assets";
 import { Link } from "react-scroll";
 import { navLinksdata } from "../../constants";
 const Navbar = () => {
-    const [showMenu, setShowMenu]=useState(false)
   return (
     <div className="w-full h-20 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
@@ -27,7 +26,6 @@ const Navbar = () => {
               >
                 {title}
               </Link>
-               {/* {title} */}
             </li>
           ))}
         </ul>
