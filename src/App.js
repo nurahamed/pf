bd637b9d132ef13daa036eeb958bd1c363e38c46
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-09-06 02:38:24 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
+import "./App.css";
+import Banner from "./components/banner/Banner";
+import Contact from "./components/contact/Contact";
+import FooterBottom from "./components/footer/FooterBottom";
+import Navbar from "./components/navbar/Navbar";
+import Projects from "./components/projects/Projects";
 
-import './App.css';
-import Banner from './components/banner/Banner';
-import Contact from './components/contact/Contact';
-import { ContactUs } from './components/contact/ContactUs';
-// import Footer from './components/footer/Footer';
-import FooterBottom from './components/footer/FooterBottom';
-import Navbar from './components/navbar/Navbar';
-import Projects from './components/projects/Projects';
-
-
-
-import Resume from './components/resume/Resume';
+import Resume from "./components/resume/Resume";
 // bg-bodyColor
 
 function App() {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
-      <Navbar/>
-      <Banner/>
-      {/* <Features/> */}
-     <Projects/>
-      <Resume/>
-      <Contact/>
-      {/* <ContactUs/> */}
-      <FooterBottom/>
+      <Navbar />
+      <Banner />
+
+      <Projects />
+      <Resume />
+      <Contact />
+
+      <FooterBottom />
     </div>
   );
 }
