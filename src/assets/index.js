afa3083779c09789c93de615b547b9b8df5ef078
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-09-06 02:02:17 +0530

    fix  photo import

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,9 +4,6 @@ import projectOne from "./images/projectOne.png";
 import projectTwo from "./images/projectTwo.png";
 import projectThree from "./images/projectThree.png";
 import projectFour from "./images/ProjectFour.jpeg";
-// import testimonialOne from "./images/testimonialOne.png";
-// import testimonialTwo from "./images/testimonialTwo.png";
-// import quote from "./images/quote.png";
 import contactImg from "./images/contact.svg";
 
 export {
@@ -16,8 +13,6 @@ export {
   projectTwo,
   projectThree,
   projectFour,
-  // testimonialOne,
-  // testimonialTwo,
-  // quote,
+ 
   contactImg,
 };
